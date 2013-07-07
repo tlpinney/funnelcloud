b7f9c2b54afa7edac595b337735111fb7bbea9d6
Travis L Pinney <travis.pinney@gmail.com>
2013-07-07 12:42:05 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# curlhdfs 
+
+    git clone http://github.com/tlpinney/curlhdfs 
+    cd curlhdfs 
+    mvn package 
+    hadoop fs -mkdir /osm-download 
+    hadoop jar target/curlhdfs-0.1.0.jar http://download.geofabrik.de/north-america/us/district-of-columbia-latest.osm.pbf /osm-download/district-of-columbia-latest.osm.pbf
---
+++ src/main/java/com/tlpinney/curlhdfs/App.java
@@ -0,0 +1,66 @@
+package com.tlpinney.curlhdfs;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+
+
+/**
+ * curlhdfs
+ *
+ */
+public class App 
+{
+	
+    public static void main( String[] args ) throws IOException
+    {
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(conf);
+        String url = args[0];
+        Path outFile = new Path(args[1]);
+        
+        	        	
+        //BufferedReader in = new BufferedReader(new FileReader(inFile));
+        //BufferedWriter outf = new BufferedWriter(new FileWriter("foobar"));
+        //BufferedOutputStream outf2 = new BufferedOutputStream(new FileOutputStream("foobar2"));
+        
+        HttpClient httpclient = new HttpClient();
+        GetMethod httpget = new GetMethod(url);
+        
+        httpclient.executeMethod(httpget);
+                
+        BufferedInputStream reader = new BufferedInputStream(
+                   httpget.getResponseBodyAsStream()); 
+               
+         //BufferedInputStream in = new BufferedInputStream(new FileInputStream(inFile));
+         //BufferedInputStream in = new BufferedInputStream(reader);
+         //reader.read
+         
+         FSDataOutputStream out = fs.create(outFile);
+        
+         int bytesRead;
+         byte[] buffer = new byte[4096];
+       
+         while ((bytesRead = reader.read(buffer)) > -1) {
+        	out.write(buffer, 0, bytesRead);
+        	//System.out.println(bytesRead);
+        }
+        //outf2.write(buffer, 0, 4096);
+        //System.out.println(bytesRead);
+             
+        //in.close();
+        reader.close();
+        out.close();
+        
+    }
+    
+    
+    
+}
