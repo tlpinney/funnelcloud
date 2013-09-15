8f1dbf39c95e9346ba76672738e61f66cd82e54d
Travis L Pinney <travis.pinney@gmail.com>
2013-09-15 19:01:48 -0400

    Fix file names

--- clouds/gdelt/gdelt.sh
+++ clouds/gdelt/gdelt.sh
@@ -5,17 +5,17 @@ cd /mnt/gdelt-staging
 
 hdfs dfs -mkdir gdelt_historical_tsv 
 for i in `cat /home/ubuntu/backfiles.txt`; do
-  curl -O http://gdelt.utdallas.edu/data/backfiles/$i 
-  unzip $i 
-  tail -n +2 $i > $i.tmp 
-  mv $i.tmp $i 
-  hdfs dfs -put $i gdelt_historical_tsv 
+  curl -O http://gdelt.utdallas.edu/data/backfiles/$i.zip
+  unzip $i.zip
+  tail -n +2 $i.csv > $i.tmp 
+  mv $i.tmp $i.csv 
+  hdfs dfs -put $i.csv gdelt_historical_tsv 
 done 
 
 hdfs dfs -mkdir gdelt_dailyupdates_tsv 
 for i in `cat /home/ubuntu/dailyupdates.txt`; do 
-  curl -O http://gdelt.utdallas.edu/data/dailyupdates/$i 
-  unzip $i 
+  curl -O http://gdelt.utdallas.edu/data/dailyupdates/$i.zip 
+  unzip $i.zip 
   tail -n +2 $i > $i.tmp 
   mv $i.tmp $i 
   hdfs dfs -put $i gdelt_dailyupdates_tsv 
