88d1144d17787f320033052ee5beed7a75d4505b
Travis L Pinney <travis.pinney@gmail.com>
2013-09-30 00:53:24 -0400

    No header. Updated docs

--- README.md
+++ README.md
@@ -6,10 +6,14 @@ More documentation here: https://github.com/tlpinney/funnelcloud/wiki
 
 ## Prequisites 
 
-CDH4.4 
+CDH4.4
+
+## Development Prequisiites
+
+protobuf 
 
 
-## Installation 
+## Installation from Package  
 
     cd /tmp
     curl -O https://github.com/tlpinney/funnelcloud/releases/download/0.1.2/funnelcloud-0.1.2-bin.tar.gz
@@ -18,5 +22,17 @@ CDH4.4
     export PATH=$PATH:/usr/local/funnelcloud-0.1.2/bin 
 
     fcl -h 
+
+
+## Installation from Git 
+
+
+    git clone https://github.com/tlpinney/funnelcloud.git
+    cd funnelcloud 
+    # generate protobuf java classes
+    protoc --java_out=src/main/java osmformat.proto 
+    protoc --java_out=src/main/java fileformat.proto 
+     
+
 
 
--- clouds/gdelt/gdelt.sh
+++ clouds/gdelt/gdelt.sh
@@ -6,23 +6,17 @@ cd /mnt/gdelt-staging
 hdfs dfs -mkdir gdelt_historical_tsv 
 for i in `cat /home/ubuntu/backfiles.txt`; do
   curl -O http://gdelt.utdallas.edu/data/backfiles/$i.zip
-  unzip $i.zip
-  tail -n +2 $i.csv > $i.tmp 
-  mv $i.tmp $i.csv 
-  hdfs dfs -put $i.csv gdelt_historical_tsv 
+  unzip $i.zip && hdfs dfs -put $i.csv gdelt_historical_tsv &  
 done 
 
 hdfs dfs -mkdir gdelt_dailyupdates_tsv 
 for i in `cat /home/ubuntu/dailyupdates.txt`; do 
   curl -O http://gdelt.utdallas.edu/data/dailyupdates/$i.zip 
-  unzip $i.zip 
-  tail -n +2 $i > $i.tmp 
-  mv $i.tmp $i 
-  hdfs dfs -put $i gdelt_dailyupdates_tsv 
+  unzip $i.zip && hdfs dfs -put $i gdelt_dailyupdates_tsv &  
 done
 
 echo "Be Patient, will take a while to load" 
-impala-shell -i localhost -f /vagrant/media/sql/create_gdelt.sql 
+#impala-shell -i localhost -f /vagrant/media/sql/create_gdelt.sql 
 
 
 
